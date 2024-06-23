0029fbd0deb368791dd30772e40f6ebafe141398
Ashish Shetty <shetty073@gmail.com>
2024-06-23 15:07:18 +0530

    fix super user creation method name to match with manage.py command

--- triptracks/identity/models/user.py
+++ triptracks/identity/models/user.py
@@ -14,7 +14,7 @@ class AppUserManager(BaseUserManager):
         return user
     
 
-    def create_super_user(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
