8b4f91480a1534b11d1e0299dc450129c61c691f
Ashish Shetty <shetty073@gmail.com>
2024-08-10 15:35:24 +0530

    add trip_service

--- triptracks/urls.py
+++ triptracks/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),
     path('identity/', include('triptracks.identity.urls')),
     path('vehicle/', include('triptracks.vehicle_service.urls')),
+    path('trip/', include('triptracks.trip_service.urls')),
 ]
